588893c9e85fa741201bcd7bc79e0a1027a11144
Mansurkhan <mansurbekeshpulatov@gmail.com>
2025-06-17 11:04:27 +0500

    add LoginPage

---
+++ src/components/UI/FormInput.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+    labelName: {
+        type: String,
+        required: true
+    },
+    inputType: {
+        type: String,
+        default: 'text'
+    },
+    placeholder: {
+        type: String,
+        default: 'Type here...'
+    }
+})
+
+const model = defineModel();
+</script>
+
+<template>
+    <label class="flex flex-col">
+        <span class="italic">{{ labelName }}</span>
+        <input
+            class="border px-2.5 py-2 rounded bg-white placeholder:text-my-gray text-black"
+            :type="inputType"
+            :placeholder
+            v-model="model"
+        />
+    </label>
+</template>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,26 +1,42 @@
 <script setup>
+import FormInput from "@/components/UI/FormInput.vue";
+import FormButton from "@/components/UI/FormButton.vue";
+import {reactive, ref} from "vue";
+import axios from "axios";
 
+const isloading = ref(false)
+
+const userData = reactive({
+    email: '',
+    password: ''
+})
+
+const auth = async () => {
+    isloading.value = true;
+
+    try {
+        const { data } = await axios.post('http://localhost:9999/api/users/auth', userData)
+
+        localStorage.setItem('token', data.token)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        isloading.value = false;
+    }
+}
 </script>
 
 <template>
     <div class="min-h-dvh grid place-content-center">
-        <form class="flex flex-col max-w-100 w-full sm:min-w-100 gap-5 bg-my-gray p-10 rounded-2xl text-white">
+        <form @submit.prevent="auth" class="flex flex-col max-w-100 w-full sm:min-w-100 gap-5 bg-my-gray p-10 rounded-2xl text-white">
             <h2 class="text-center text-2xl font-medium">Kirish</h2>
-            <label class="flex flex-col">
-                <span>Email</span>
-                <input class="border px-2.5 py-2 rounded bg-white placeholder:text-my-gray hover:text-black" type="text" placeholder="Email..."/>
-            </label>
-            <label class="flex flex-col">
-                <span>Parol</span>
-                <input class="border px-2.5 py-2 rounded bg-white placeholder:text-my-gray hover:text-black" type="password" placeholder="Password..."/>
-            </label>
-            <button class=" lg:inline px-8 py-2 bg-my-gray rounded-lg hover:bg-white hover:text-my-blue-gray cursor-pointer transition-all">
-                Kirish
-            </button>
+
+            <pre>{{userData}}</pre>
+
+            <FormInput v-model="userData.email" input-type="email" label-name="Email" placeholder="Email kiriting..." />
+            <FormInput v-model="userData.password" input-type="password" label-name="Parol" placeholder="Parol kiriting..." />
+
+            <FormButton :isloading :disabled="isloading">Kirish</FormButton>
         </form>
     </div>
 </template>
-
-<style scoped>
-
-</style>
